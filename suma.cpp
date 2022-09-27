325c5e43e8cfb9295acbd826d6b2780d177f23f7
nais1998 <anais.napa.ortiz@utelvt.edu.ec>
2022-09-27 10:35:40 -0500

    programa de suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+ using namespace std; 
+ int main() 
+ { 
+         float k,p,o; 
+         cout<<"ingrese primer  numeros k=";cin>>k; 
+         cout<<"ingrese el segundo numero p=";cin>>p; 
+         o=k+p; 
+         cout<<"El resultado es "<<k<<" + "<<p<<"="<<o<<endl; 
+  
+         return 0; 
+ }
