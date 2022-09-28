896bef0f922175d77a02a8e56169671bbeabbc24
nais1998 <anais.napa.ortiz@utelvt.edu.ec>
2022-09-28 15:42:57 -0500

    programa de calculadora.cpp modificado

---
+++ calculadora.cpp
@@ -0,0 +1,26 @@
+//anais stefany napa ortiz
+//modificacion 28-09-2022
+#include<iostream> 
+ using namespace std; 
+  
+ int main() 
+ { 
+         float a,e,s,m,d,r; 
+         //Ingreso de datos 
+         cout<<"Ingrese en valor de a=:"; 
+         cin>>a; 
+         cout<<"Ingrese en valor de e=:"; 
+         //Operaciones  
+         cin>>e; 
+         s=a+e; 
+         m=a*e; 
+         d=a/e; 
+         r=a-e; 
+         //Muestra los resutaldos. 
+         cout<<"Las suma de "<<a<<" + "<<e<<" = "<<s<<endl; 
+         cout<<"Las multiplicacion de "<<a<<" * "<<e<<" = "<<m<<endl; 
+         cout<<"Las division de "<<a<<" / "<<e<<" = "<<d<<endl; 
+         cout<<"Las resta de "<<a<<" - "<<e<<" = "<<r<<endl; 
+         return 0 ; 
+  
+ }
