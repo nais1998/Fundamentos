33ff4d17258f9f3aa16dd88d944912069a763559
nais1998 <anais.napa.ortiz@utelvt.edu.ec>
2022-09-28 16:46:03 -0500

    programa de velocimetro.cpp modificado

---
+++ velocimetro.cpp
@@ -0,0 +1,17 @@
+//anaisstefanynapaortiz
+//modificado 28-09-2022
+#include<iostream> 
+ using namespace std; 
+  
+ int main() 
+ { 
+         float a,s,v; 
+         cout<<"Ingrese la distancia(en km) a= "; cin>>a; 
+         cout<<"Ingrese el tiempo(en horas) s= "; cin>>s; 
+         v=a/s; 
+         cout<<"La velocidad calculada es de v= "<<v<<endl<<endl; 
+         return 0; 
+  
+  
+  
+ }
