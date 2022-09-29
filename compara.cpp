6f8a24d3c39d79b1d54d478f3e3d3180c5486e6d
nais1998 <anais.napa.ortiz@utelvt.edu.ec>
2022-09-28 19:26:42 -0500

    programa de compara.cpp modificado

---
+++ compara.cpp
@@ -0,0 +1,28 @@
+//anaisstefanynapaortiz
+//modificar 28-09-2022
+#include<iostream> 
+ using namespace std; 
+ int main() 
+ { 
+         float n,o; 
+         cout<<"ingrese n o:"; cin>>n>>o; 
+         if(n>o) 
+         { 
+           cout<<n<<" es mayor n "<<o<<endl; 
+  
+  
+         }else{ 
+  
+           cout<<o<<" es mayor n "<<n<<endl; 
+  
+  
+         } 
+  
+         return 0; 
+  
+  
+  
+  
+  
+  
+ }
